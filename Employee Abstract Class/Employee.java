cbd76d08dd68ab99ba6900c0691ac9ba0d75e88c
Yap Joon Lok <73649534+lok2042@users.noreply.github.com>
2021-07-14 15:18:53 +0800

    Create Employee.java

---
+++ Employee Abstract Class/Employee.java
@@ -0,0 +1,22 @@
+public abstract class Employee {
+	
+	private String name;
+	
+	public Employee(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {
+		return "Employee[name: " + name + "]";
+	}
+	
+	public abstract double getPayment();
+}
