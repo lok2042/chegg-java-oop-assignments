2fdec2340d8e77c0e817a1b11b417be804101c62
Yap Joon Lok <73649534+lok2042@users.noreply.github.com>
2021-07-14 15:16:06 +0800

    Create CommissionEmployee.java

---
+++ Employee Abstract Class/CommissionEmployee.java
@@ -0,0 +1,37 @@
+  
+public class CommissionEmployee extends Employee {
+	
+	private double commissionRate;
+	private double totalSales;
+	
+	public CommissionEmployee(String name, double rate, double sales) {
+		super(name);
+		this.commissionRate = rate;
+		this.totalSales = sales;
+	}
+	
+	public double getCommissionRate() {
+		return commissionRate;
+	}
+
+	public void setCommissionRate(double commissionRate) {
+		this.commissionRate = commissionRate;
+	}
+	
+	public double getTotalSales() {
+		return totalSales;
+	}
+
+	public void setTotalSales(double totalSales) {
+		this.totalSales = totalSales;
+	}
+
+	@Override
+	public double getPayment() {
+		return totalSales * commissionRate;
+	}
+	
+	public String toString() {
+		return "CommissionEmployee[name: " + this.getName() + ", rate: " + (commissionRate * 100) + "%, sales: $" + totalSales + "]";
+	}
+}
